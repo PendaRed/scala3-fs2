b8ed7828f871c3fd7136d7200d5c0875fd4df85c
Jonathan Gibbons <gibbons_jonathan@hotmail.com>
2022-06-08 08:09:35 +0100

    More util examples

--- src/main/scala/com/jgibbons/fs2/zcribsheet/StreamingValuesBasedOnPrevious.scala
+++ src/main/scala/com/jgibbons/fs2/zcribsheet/StreamingValuesBasedOnPrevious.scala
@@ -6,10 +6,10 @@ import fs2.Pure
 @main def StreamingValuesBasedOnPrevious =
   import cats.effect.unsafe.implicits.global
 
+  // Also note: fs2.Stream.constant(1).scan(0)(_+_) will give 1,2,3,4
   val seven: fs2.Stream[Pure, Int] = fs2.Stream.iterate(1)(_*2).take(7)
   val reversed: fs2.Stream[Pure, Int] = fs2.Stream.iterate(64)(_/2).take(7)
 
-  (seven ++ reversed)
-    .evalTap(v=>IO{println(v)})
+  (seven ++ reversed).evalTap(v=>IO{println(v)})
     .compile.drain
     .unsafeRunSync
